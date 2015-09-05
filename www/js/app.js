0aa384b1a3e71ebbcaea9bbd64852338fcd8f6db
Jhony Lucas <jhonyjl37@gmail.com>
2015-09-04 23:02:55 -0300

    chat firebase example

---
+++ www/js/app.js
@@ -0,0 +1,35 @@
+  // Ionic Starter App
+
+  // angular.module is a global place for creating, registering and retrieving Angular modules
+  // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+  // the 2nd parameter is an array of 'requires'
+  var app = angular.module('starter', ['ionic','firebase']);
+
+  app.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  });
+
+  app.controller("SampleCtrl", function($scope, $firebaseArray) {
+    var ref = new Firebase("https://chat-prot.firebaseio.com/messages");
+    // create a synchronized array
+    // click on `index.html` above to see it used in the DOM!
+    $scope.messages = $firebaseArray(ref);
+
+
+    $scope.addMessage = function() {
+      $scope.messages.$add({
+        text: $scope.message
+      });
+       $scope.message = "";
+    };
+
+  });
